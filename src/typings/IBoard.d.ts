2795db3eee83b21349d513978b9ee780d231cc76
Sergio Lazaro Mondargo <sergiolazaromondargo@gmail.com>
2023-06-19 18:45:56 -0500

    type

--- src/typings/IBoard.d.ts
+++ src/typings/IBoard.d.ts
@@ -1,19 +1,25 @@
 export interface IAuthor {
-    displayName?: string;
-    username?: string;
-    id?: number | string;
-    avatarURL?: string;
+  displayName?: string;
+  username?: string;
+  id?: number | string;
+  avatarURL?: string;
 }
+
 export interface IBoard {
-    name?: string;
-    description?: string;
-    followersCount?: number;
-    boardURL?: string;
-    id?: string | number;
-    sectionsCount?: number;
-    createdAt?: string;
-    coverURL?: string;
-    lastUpdatedAt?: string;
-    pinCounts?: number;
-    author?: IAuthor;
+  name?: string;
+  description?: string;
+  followersCount?: number;
+  boardURL?: string;
+  id?: string | number;
+  pins?: Array<{
+    id: string;
+    type: string;
+    trackingParams: string;
+  }>;
+  sectionsCount?: number;
+  createdAt?: string;
+  coverURL?: string;
+  lastUpdatedAt?: string;
+  pinCounts?: number;
+  author?: IAuthor;
 }
